aa7dbafb543954e6b6e8c71bb963aa54818e0c66
Vatsal Patel <62203962+VatsalPatel18@users.noreply.github.com>
2024-05-06 18:05:05 +0530

    CBIO portal Data Load

--- data_load.py
+++ data_load.py
@@ -2,8 +2,9 @@ import os
 import pandas as pd
 
 class BasicPreProcess:
-    def __init__(self, cancer_type):
+    def __init__(self, cancer_type,out_loc=None):
         self.cancer_type = cancer_type
+        self.out_dir = f'{out_loc}/{self.cancer_type}'
         self.preprocess_gene_expression()
         self.preprocess_normal_gene_expression()
         self.preprocess_cna_data()
@@ -20,8 +21,8 @@ class BasicPreProcess:
                  .set_index(['GENES', 'Entrez_Gene_Id'])
                  )
 
-        out_dir = f'Nov8/{self.cancer_type}'
-        os.makedirs(out_dir, exist_ok=True)
+        # out_dir = f'{sefl.out_loc}/{self.cancer_type}'
+        os.makedirs(self.out_dir, exist_ok=True)
         ge_df.to_csv(f'{out_dir}/ge_df.csv')
 
     def preprocess_normal_gene_expression(self):
@@ -36,8 +37,7 @@ class BasicPreProcess:
             .set_index(['GENES', 'Entrez_Gene_Id'])
         )
 
-        out_dir = f'Nov8/{self.cancer_type}'
-        os.makedirs(out_dir, exist_ok=True)
+        os.makedirs(self.out_dir, exist_ok=True)
         normal_ge_df.to_csv(f'{out_dir}/normal_ge_df.csv')
 
     def preprocess_cna_data(self):
@@ -53,9 +53,8 @@ class BasicPreProcess:
             .drop_duplicates()
         )
 
-        out_dir = f'Nov8/{self.cancer_type}'
-        os.makedirs(out_dir, exist_ok=True)
-        cna_df.to_csv(f'{out_dir}/cna_df.csv', index=False)
+        os.makedirs(self.out_dir, exist_ok=True)
+        cna_df.to_csv(f'{self.out_dir}/cna_df.csv', index=False)
 
     def preprocess_methylation_data(self):
         path = f'TCGA/Extract/{self.cancer_type}_tcga_pan_can_atlas_2018/data_methylation_hm27_hm450_merged.txt'
@@ -68,9 +67,9 @@ class BasicPreProcess:
         df_pivoted = df_melted.pivot_table(index=['PatientID', 'ENTITY_STABLE_ID', 'NAME'],
                                            columns='DESCRIPTION', values='Value', fill_value=0).reset_index()
         df_pivoted.rename(columns={'ENTITY_STABLE_ID': 'Entrez_Gene_Id', 'GENES': 'Entrez_Gene_Id'}, inplace=True)
-        out_dir = f'Nov8/{self.cancer_type}'
-        os.makedirs(out_dir, exist_ok=True)
-        df_pivoted.to_csv(f'{out_dir}/meth_pivot.csv', index=False)
+
+        os.makedirs(self.out_dir, exist_ok=True)
+        df_pivoted.to_csv(f'{self.out_dir}/meth_pivot.csv', index=False)
 
     def preprocess_mutation_data(self):
         path = f'TCGA/Extract/{self.cancer_type}_tcga_pan_can_atlas_2018/data_mutations.txt'
@@ -84,10 +83,6 @@ class BasicPreProcess:
         encoded_df = encoded_df.rename(columns={col: col[23:] for col in encoded_df.columns[3:]})
         encoded_df.columns = [col.split('_')[-1] if 'Variant_Classification' in col else col for col in encoded_df.columns]
         encoded_df2 = encoded_df.groupby(['PatientID', 'GENES', 'Entrez_Gene_Id']).sum().reset_index()
-        out_dir = f'Nov8/{self.cancer_type}'
-        os.makedirs(out_dir, exist_ok=True)
-        encoded_df2.to_csv(f'{out_dir}/mut_encoded_df.csv', index=False)
 
-# Example usage:
-cancer_preprocessor = BasicPreProcess(cancer_type='lusc')
-
+        os.makedirs(self.out_dir, exist_ok=True)
+        encoded_df2.to_csv(f'{self.out_dir}/mut_encoded_df.csv', index=False)
